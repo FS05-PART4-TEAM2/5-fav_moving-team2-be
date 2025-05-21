27aa6e0cf28a002a24d82bbc5ab6e4cd0be13f82
seha <hansaemj20@gmail.com>
2025-05-21 14:19:15 +0900

    infra fix: app.module.ts 파일 내 .env 수정

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,7 @@ import { MoverModule } from "./mover/mover.module";
     ConfigModule.forRoot({
       isGlobal: true, // 모든 모듈에서 import 없이 사용 가능
       // envFilePath: `.env`, // 또는 환경에 따라 다른 파일명을 사용
+      envFilePath: [".env", `.env.${process.env.NODE_ENV}`],
     }),
     DatabaseModule,
     CustomerModule,
