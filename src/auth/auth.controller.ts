aa2b49ffd7d1d6757e6b2391c5360b1c2ade7ed0
hyuk-dev <hyuk.development@gmail.com>
2025-05-22 14:59:57 +0900

    feat: cookie 저장

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,26 @@
-import { BadRequestException, Controller, Get, Param, Req, Res, UseGuards } from "@nestjs/common";
+import {
+  BadRequestException,
+  Controller,
+  Get,
+  Param,
+  Req,
+  Res,
+  UseGuards,
+} from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { AuthGuard } from "@nestjs/passport";
 import { Request, Response } from "express";
 import { ConfigService } from "@nestjs/config";
 import { CustomerAuthService } from "src/customer/auth/auth.service";
 import { ApiResponse } from "src/common/dto/api-response.dto";
-import { CustomerOauthLoginResponseDto, MoverOauthLoginResponseDto } from "src/customer/auth/dto/oauthLogin.dto";
+import {
+  CustomerOauthLoginResponseDto,
+  MoverOauthLoginResponseDto,
+} from "src/customer/auth/dto/oauthLogin.dto";
 import { SafeCustomer } from "src/customer/types/customerWithoutPw";
 import { SafeMover } from "src/customer/types/moverWithoutPw";
 import { MoverAuthService } from "src/mover/auth/auth.service";
+import { SetAuthCookies } from "src/common/utils/set-auth-cookies.util";
 
 @Controller("api/auth")
 export class AuthController {
@@ -37,17 +49,26 @@ export class AuthController {
 
   @Get("google/redirect")
   @UseGuards(AuthGuard("google"))
-  async googleRedirect(@Req() req: Request): Promise<ApiResponse<SafeCustomer | SafeMover>> {
+  async googleRedirect(
+    @Req() req: Request,
+    @Res({ passthrough: true }) res: Response,
+  ): Promise<ApiResponse<SafeCustomer | SafeMover>> {
     let userInfo: CustomerOauthLoginResponseDto | MoverOauthLoginResponseDto;
     //req.user의 role에 따라 분기처리 예정
-    if(req.user?.role === 'customer') {
+    if (req.user?.role === "customer") {
       // 손님 OAuth 로그인 일 때
-      userInfo = await this.customerAuthService.signUpOrSignInByOauthCustomer(req.user);
+      userInfo = await this.customerAuthService.signUpOrSignInByOauthCustomer(
+        req.user,
+      );
+      SetAuthCookies.set(res, userInfo.accessToken, userInfo.refreshToken);
       return ApiResponse.success(userInfo.customer, "로그인 완료");
     }
-    if(req.user?.role === 'mover') {
+    if (req.user?.role === "mover") {
       // 기사 OAuth 로그인 일 때
-      userInfo = await this.moverAuthService.signUpOrSignInByOauthMover(req.user);
+      userInfo = await this.moverAuthService.signUpOrSignInByOauthMover(
+        req.user,
+      );
+      SetAuthCookies.set(res, userInfo.accessToken, userInfo.refreshToken);
       return ApiResponse.success(userInfo.mover, "로그인 완료");
     }
     throw new BadRequestException();
