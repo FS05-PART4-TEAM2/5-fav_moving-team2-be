79cfc87cb6fe5f6e4bb6e76cc12b4f0571849cb5
seha <hansaemj20@gmail.com>
2025-05-27 17:25:37 +0900

    fix: 쿠키 secure, sameSite 조합 변경

--- src/common/utils/set-auth-cookies.util.ts
+++ src/common/utils/set-auth-cookies.util.ts
@@ -7,18 +7,22 @@ export class SetAuthCookies {
     accessToken: string,
     refreshToken: string,
   ) {
-    const origin = req.headers.origin;
-    const isLocal = origin?.startsWith("http://localhost");
+    const origin = req.headers.origin ?? "";
+    const apiProtocol = req.protocol; // 'http' or 'https'
+    const isLocal = origin.startsWith("http://localhost");
 
-    res.cookie("accessToken", accessToken, {
+    const isSecure = apiProtocol === "https" && !isLocal;
+    const sameSite: "lax" | "none" = isLocal ? "lax" : "none";
+
+    console.log({ origin, apiProtocol, isLocal, isSecure, sameSite });
+
+    const cookieOptions = {
       httpOnly: true,
-      secure: !isLocal,
-      sameSite: isLocal ? "lax" : "none",
-    });
-    res.cookie("refreshToken", refreshToken, {
-      httpOnly: true,
-      secure: !isLocal,
-      sameSite: isLocal ? "lax" : "none",
-    });
+      secure: isSecure,
+      sameSite,
+    } as const;
+
+    res.cookie("accessToken", accessToken, cookieOptions);
+    res.cookie("refreshToken", refreshToken, cookieOptions);
   }
 }
