f81f78a85161d3d0a7842c3a67f45c3af63bb6bc
hyuk-dev <hyuk.development@gmail.com>
2025-06-03 22:23:49 +0900

    항상 ssl

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -19,25 +19,7 @@ import * as path from "path";
           username: config.get<string>("DB_USERNAME"),
           password: config.get<string>("DB_PASSWORD"),
           database: config.get<string>("DB_DATABASE"),
-          ssl:
-            isProduction && useSSL
-              ? (() => {
-                  try {
-                    const certPath = path.join(
-                      __dirname,
-                      "..",
-                      "..",
-                      "rds-combined-ca-bundle.pem",
-                    );
-                    return { ca: fs.readFileSync(certPath).toString() };
-                  } catch (err) {
-                    console.warn(
-                      "SSL 인증서 파일이 없습니다. ssl: false 로 fallback합니다.",
-                    );
-                    return false;
-                  }
-                })()
-              : useSSL,
+          ssl: true,
           entities: [__dirname + "/../**/*.entity{.ts,.js}"],
           synchronize: !isProduction,
         };
