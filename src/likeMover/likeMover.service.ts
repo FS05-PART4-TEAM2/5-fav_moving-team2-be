6c9d705b88f345e05fa9257f8ca9d8953fa65b74
hyuk-dev <hyuk.development@gmail.com>
2025-06-10 13:40:34 +0900

    feat: 찜한 기사 리스트 조회 API

--- src/likeMover/likeMover.service.ts
+++ src/likeMover/likeMover.service.ts
@@ -8,6 +8,11 @@ import { InjectRepository } from "@nestjs/typeorm";
 import { DataSource, Repository } from "typeorm";
 import { LikeMover } from "./likeMover.entity";
 import { Mover } from "src/mover/mover.entity";
+import { GetLikeMoverData } from "./dto/get-like-mover-list.response.dto";
+import {
+  PaginatedScrollDto,
+  PaginatedScrollResponseDto,
+} from "src/common/dto/pagination.dto";
 
 @Injectable()
 export class likeMoverService {
@@ -131,5 +136,70 @@ export class likeMoverService {
     } finally {
       await queryRunner.release();
     }
+  }
+
+  async getLikeMoverList(
+    user: { userId: string; userType: string },
+    paginationScrollDto: PaginatedScrollDto,
+  ): Promise<PaginatedScrollResponseDto<GetLikeMoverData>> {
+    const { userId, userType } = user;
+
+    const page = Math.max(paginationScrollDto.page, 1);
+    const limit = paginationScrollDto.limit || 8;
+    const skip = (page - 1) * limit;
+
+    if (userType !== "customer") {
+      throw new UnauthorizedException("손님 전용 API입니다.");
+    }
+
+    const likedMovers = await this.likeRepository
+      .createQueryBuilder("like")
+      .innerJoin("mover", "mover", "like.moverId = mover.id::text")
+      .where("like.customerId = :userId", { userId })
+      .andWhere("mover.isProfile = true")
+      .select([
+        `mover.id AS "id"`,
+        `mover.idNum AS "idNum"`,
+        `mover.nickname AS "nickName"`,
+        `mover.profileImage AS "profileImage"`,
+        `mover.serviceList AS "serviceList"`,
+        `mover.likeCount AS "likeCount"`,
+        `mover.totalRating / NULLIF(mover.reviewCounts, 0) AS "totalRating"`, // 평균 별점
+        `mover.reviewCounts AS "reviewCounts"`,
+        `mover.intro AS "intro"`,
+        `mover.career AS "career"`,
+        `mover.confirmedCounts AS "confirmedCounts"`,
+        `mover.createdAt AS "createdAt"`,
+      ])
+      .addSelect("true", "isLiked")
+      .addSelect(
+        `CASE
+            WHEN EXISTS (
+              SELECT 1 FROM assign_mover assign
+              WHERE assign."customerId" = :userId
+                AND assign."moverId" = mover.id::text
+            )
+            THEN true
+            ELSE false
+          END`,
+        "isAssigned",
+        )
+      .setParameter("userId", userId)
+      .take(limit)
+      .skip(skip)
+      .orderBy("mover.createdAt", "DESC")
+      .getRawMany();
+
+    const total = await this.likeRepository
+      .createQueryBuilder("like")
+      .where("like.customerId = :userId", { userId })
+      .getCount();
+
+    return new PaginatedScrollResponseDto<GetLikeMoverData>(
+      likedMovers,
+      total,
+      page,
+      limit,
+    );
   }
 }
